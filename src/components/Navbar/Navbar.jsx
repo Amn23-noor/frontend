9a08498b5ea7e38cf394736d12ecab9e8ca8d696
Amna Zafar <amnan2710@gmail.com>
2024-07-07 00:45:32 +0500

    2nd commiy

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import './navbar.css'
+import logo from '../assets/favicon.png'
+import cart_icon from '../assets/cart_icon.png'
 const Navbar = () => {
+    const [menu,Setmenu] = useState("Shop")
   return (
-    <div>Navbar</div>
+    <div className='navbar'>
+  <div className='nav_logo'>
+    <img src={logo} width={30} alt="Logo"/>
+    <p>Shirtfy</p>
+  </div>
+  <ul className="nav_menu">
+  <li onClick={()=>{Setmenu('shop')}}>Shop <hr/></li>
+    <li onClick={()=>{Setmenu('mens')}}>Men</li>
+    <li onClick={()=>{Setmenu('womens')}}>Women</li>
+    <li onClick={()=>{Setmenu('kids')}}>Kids</li>
+  </ul>
+  <div className="nav-login-cart">
+    <button>Login</button>
+    <img src={cart_icon} alt="Cart" />
+    <div className="nav_count">0</div>
+  </div>
+</div>
   )
 }
 
